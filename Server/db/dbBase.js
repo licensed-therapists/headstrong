aac5acb14dd921aa5ad40cb8d5246acabb201299
Edward Ellington <eellin6@gmail.com>
2021-01-30 14:59:40 -0600

    add/ database connected to server - schema set up

--- Server/db/dbBase.js
+++ Server/db/dbBase.js
@@ -4,5 +4,20 @@ const db = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: '',
-  database: 'headstrong'
+  database: 'journals'
 });
+
+
+const getAllJournals = () => {
+  return new Promise((resolve, reject) => {
+    db.query('SELECT * FROM entries', (err, results) => {
+      if (err) { return reject(err); }
+      resolve(results);
+    });
+  });
+};
+
+
+module.exports = {
+  getAllJournals
+}
--- schema.sql
+++ schema.sql
@@ -6,9 +6,17 @@ USE journals;
 
 CREATE TABLE entries (
   id int NOT NULL AUTO_INCREMENT,
-  username varchar(255) NOT NULL,
-  title varchar(25) NOT NULL,
+  username varchar(25) NOT NULL,
+  title varchar(255) NOT NULL,
   body varchar(1000) NOT NULL,
-  journal_image BLOB,
+  journal_image BLOB NOT NULL,
+  time_stamp varchar(100) NOT NULL,
+  quote varchar(255) NOT NULL,
+  weather varchar(100) NOT NULL,
+  mood varchar(255) NOT NULL,
+  comments varchar(255) NOT NULL,
+
   PRIMARY KEY (id)
 );
+
+INSERT into entries (username, title, body, journal_image, time_stamp, quote, weather, mood, comments) VALUES ("ian549", "sunday morning blues", "I've been feeling up this morning", "https://pbs.twimg.com/media/EmdsOxSXEAE224X.jpg", "saturday, Jan 30th,2021", "You only live once", "cloudy", "good", "let me read your diary");
