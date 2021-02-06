3323e5b133e570546cfe24ce4fea2fe8c7ad96b1
Ian Wright <iancoder1097@gmail.com>
2021-02-06 14:51:27 +0000

    ignore this commit

--- Server/helpers/geolocation.js
+++ Server/helpers/geolocation.js
@@ -4,7 +4,7 @@ const config = require('../config');
 const getGeolocationByIP = (userIP) => {
 
   // const url = `http://api.ipstack.com/${userIP}?access_key=${userIP}`;
-  const url = `http://api.ipstack.com/${userIP}?access_key=09865bbb6e7031638d0e5f02fd683e52`;
+  const url = `http://api.ipstack.com/${userIP}?access_key=${process.env.access_key}`;
 
   return axios.get(url)
     .then(({ data }) => data)
