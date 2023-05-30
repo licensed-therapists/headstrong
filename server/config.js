196c5e0aeb8b42156744cfd0cbf2a2b55fb97b1e
Emmy Bishop <miaui96@gmail.com>
2023-05-30 15:13:25 -0500

    Update config.js to export OpenAI token

--- server/config.js
+++ server/config.js
@@ -1,6 +1,7 @@
-const { WEATHERBIT_TOKEN, IPSTACK_TOKEN } = process.env;
+const { WEATHERBIT_TOKEN, IPSTACK_TOKEN, OPENAI_TOKEN } = process.env;
 
 module.exports = {
   WEATHERBIT_TOKEN,
-  IPSTACK_TOKEN
+  IPSTACK_TOKEN,
+  OPENAI_TOKEN
 };
