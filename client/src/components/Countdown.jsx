dcd0c11a6c7b7255a651ac648c1f75f1c009bff2
Emmy Bishop <miaui96@gmail.com>
2023-05-30 18:27:12 -0500

    Add basic countdown functionality

--- client/src/components/Countdown.jsx
+++ client/src/components/Countdown.jsx
@@ -1,14 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Countdown = ({ story, setStory }) => {
+const Countdown = () => {
   const [event, setEvent] = useState('');
   const [task, setTask] = useState('');
+  const [date, setDate] = useState('');
+  const [story, setStory] = useState('');
+  const [countdown, setCountdown] = useState('');
 
   const handleEventChange = (e) => {
     const { value } = e.target;
     setEvent(value);
-    console.log(event);
   }
 
   const handleTaskChange = (e) => {
@@ -27,17 +29,48 @@ const Countdown = ({ story, setStory }) => {
     }
   }
 
-  // useEffect(() => {
-  //   setText(text);
-  // }, [text])
+  const handleDateChange = (e) => {
+    const { value } = e.target;
+    const [year, month, day] = value.split('-');
+    const newDate = new Date(year, month - 1, day); // month is zero-based
+    setDate(newDate);
+  };
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      if (date) {
+        const now = new Date();
+        const timeDifference = date.getTime() - now.getTime();
+  
+        if (!timeDifference) {
+          setStory('Countdown is over!');
+          clearInterval(intervalId);
+        } else {
+          const days = Math.floor(timeDifference / (1000 * 60 * 60 * 24));
+          const hours = Math.floor((timeDifference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          const minutes = Math.floor((timeDifference % (1000 * 60 * 60)) / (1000 * 60));
+          const seconds = Math.floor((timeDifference % (1000 * 60)) / 1000);
+  
+          setCountdown(`Countdown: ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds`);
+        }
+      }
+    }, 1000);
+  
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, [date]);
+  
 
   return (
     <div>
       <h1>Countdown</h1>
       Event:<input type="text" onChange={handleEventChange}></input>
       Task:<input type="text" onChange={handleTaskChange}></input>
+      Date:<input type="date" onChange={handleDateChange}></input>
       <button type="submit" onClick={handleSubmit}>Submit</button>
-      <div color="black">{story ? story : null}</div>
+      <div>{story ? story : null}</div>
+      <div>{countdown ? countdown : null}</div>
     </div>
   )
 }
