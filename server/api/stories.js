5b07c54c7416511054e349e69a33d053eab376a0
Emmy Bishop <miaui96@gmail.com>
2023-05-31 13:17:25 -0500

    Add functionality to save info to db

--- server/api/stories.js
+++ server/api/stories.js
@@ -1,11 +1,15 @@
 const { Router } = require('express');
 const Stories = Router();
 const { getStory } = require('../helpers/stories');
+const { addCountdown } = require('../db/index');
 
 Stories.post('/', async (req, res) => {
-  const { event, task } = req.body;
+  const { event, task, date } = req.body;
+  const { Headstrong } = req.cookies;
   try {
     const story = await getStory(event, task);
+    const { text } = story.choices[0];
+    await addCountdown(Headstrong, event, date, text);
     res.status(201).send(story);
   } catch (err) {
     console.error('Failed to POST text to API:', err);
--- server/db/index.js
+++ server/db/index.js
@@ -28,25 +28,27 @@ const Countdown = sequelize.define('countdowns', {
     type: Sequelize.STRING,
   },
   date: {
-    type: Sequelize.DATE,
+    type: Sequelize.STRING,
   },
   story: {
-    type: Sequelize.STRING,
+    type: Sequelize.TEXT('long'),
   }
 });
-Countdown.sync();
+Countdown.sync({alter: true});
 
-const addCountdown = async(body, user) => {
-  const { event, date, story } = body;
-
-  const newCountdown = await Countdown.create({
-    username: user,
-    eventName: event,
-    date,
-    story
-  });
-
-  return newCountdown.save();
+const addCountdown = async(user, event, date, story) => {
+  // const { event, date, story } = body;
+  try {
+    const newCountdown = await Countdown.create({
+      username: user,
+      eventName: event,
+      date,
+      story
+    });
+    return newCountdown.save();
+  } catch (err) {
+    console.error('Failed to SAVE countdown to db:', err);
+  }
 };
 
 
@@ -153,5 +155,6 @@ module.exports = {
   deleteJournal,
   updateJournal,
   Entries,
-  Countdown
+  addCountdown,
+  Countdown,
 };
