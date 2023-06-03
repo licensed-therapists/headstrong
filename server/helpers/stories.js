977c1234c488905668bee65f350cd18ebbe56ce8
Emmy Bishop <miaui96@gmail.com>
2023-06-03 13:43:44 -0500

    Edit OpenAI prompt

--- server/helpers/stories.js
+++ server/helpers/stories.js
@@ -11,7 +11,7 @@ const openai = new OpenAIApi(configuration);
 const getStory = async (event, task, stressors) => {
   const response = await openai.createCompletion({
     model: "text-davinci-002",
-    prompt: `I'm worried about an upcoming ${event} where I have to ${task}. It's stressful for this reason: ${stressors}. Please write me a story in second person perspective that describes a worst-case scenario for how this event could play out. The story should be roughly 50 words in length.`,
+    prompt: `I'm worried about an upcoming ${event} where I have to ${task}. It's stressful for this reason: ${stressors}. Please write me a story in second person perspective that describes a worst-case scenario for how this event will play out. The story should be roughly 50 words in length.`,
     max_tokens: 200,
     temperature: 1.30,
   });
